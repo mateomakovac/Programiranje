dcdc28dac98f8bf93d9ed5c0ebbcebd6048633f2
mateomakovac <153857861+mateomakovac@users.noreply.github.com>
2024-01-19 00:14:07 +0100

    Add files via upload

---
+++ Inicialno_Ponavljanje/Inicialno_Ponavljanje/Program2.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+internal class Program2
+{
+    static void Main2(string[] args)
+    {
+        {
+            Console.WriteLine("Unesite niz znakova:");
+            string inputString = Console.ReadLine();
+          
+            string modifiedString = inputString.Replace(' ', '_');
+
+            Console.WriteLine("Izmijenjeni niz znakova:");
+            Console.WriteLine(modifiedString);
+            Console.ReadKey();
+        }
+
+    }
+}
