35888813e22ccd9d69683bd1681c133bf2bb5d0a
mateomakovac <153857861+mateomakovac@users.noreply.github.com>
2024-01-25 11:10:31 +0100

    Add files via upload

---
+++ Nasljeđivanje/Nasljeđivanje/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nasljeđivanje
+{
+
+
+    class Cat
+    {
+        public bool opasna;
+
+        public bool Opasna { get => opasna; set => opasna = value; }
+
+
+
+        public virtual void opasnost()
+        {
+            Opasna = false;
+        }
+
+
+    }
+
+
+
+    class Gepard : Cat
+    {
+        public override void opasnost()
+        {
+            Opasna = true;
+        }
+
+    }
+
+
+
+
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Gepard g = new Gepard();
+            g.opasnost();
+
+            Console.WriteLine(g.Opasna);
+            Console.ReadKey();
+        }
+
+        
+
+    }
+}
