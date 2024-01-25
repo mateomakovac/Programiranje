77a9ca66fb27e428ef1aef5450f32643052d5e6d
mateomakovac <153857861+mateomakovac@users.noreply.github.com>
2024-01-25 11:21:31 +0100

    Add files via upload

---
+++ NebeskoTijelo/NebeskoTijelo/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NebeskoTijelo
+{
+
+    class NebeskoTijelo
+    {
+        protected double BrzinaOkoOsi;
+        protected double BrzinaOkOSunca;
+    }
+
+
+        class Planet : NebeskoTijelo
+     {
+        public Planet(double d1, double d2)
+        {
+            BrzinaOkoOsi = d1;
+            BrzinaOkOSunca = d2;
+        }
+
+    }
+     class Satelit : NebeskoTijelo
+        {
+            public Satelit(double d1,double d2)
+            {
+                BrzinaOkOSunca = d2;
+                BrzinaOkoOsi = d1;
+            }
+
+
+
+        
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Planet Zemlja = new Planet(1000, 2000);
+            Satelit Mjesec = new Satelit(100, 200);
+        }
+
+
+    }
+}
