001727d952aa63f74ed4620c4a1a45a5266280e0
mateomakovac <153857861+mateomakovac@users.noreply.github.com>
2024-03-14 19:39:35 +0100

    Add files via upload

---
+++ LINQ_Mateo_Makovac_3.2/LINQ_Mateo_Makovac_3.2/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ_Mateo_Makovac_3._2
+{
+    class Student
+    {
+        public int StudentID { get; set; }
+        public string StudentName { get; set; }
+        public int Age { get; set; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student[] studentArray = {
+            new Student() { StudentID = 1, StudentName = "John", Age = 18 },
+            new Student() { StudentID = 2, StudentName = "Steve",  Age = 21 },
+            new Student() { StudentID = 3, StudentName = "Bill",  Age = 25 },
+            new Student() { StudentID = 4, StudentName = "Ram" , Age = 20 },
+            new Student() { StudentID = 5, StudentName = "Ron" , Age = 31 },
+            new Student() { StudentID = 6, StudentName = "Chris",  Age = 17 },
+            new Student() { StudentID = 7, StudentName = "Rob",Age = 19  },
+        };
+
+            Student[] students = new Student[10];
+
+            int i = 0;
+
+            var studenti = from s in studentArray
+                           where s.Age > 12 && s.Age < 20
+                           orderby s.Age descending
+                           select s;
+
+            foreach (var stu in studenti)
+            {
+                Console.WriteLine("Ime: {0} Godine: {1} ", stu.StudentName, stu.Age);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
