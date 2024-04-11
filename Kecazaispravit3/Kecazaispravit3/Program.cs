de9c1a241e392170ebadaab45e5c07f908ac1186
mateomakovac <153857861+mateomakovac@users.noreply.github.com>
2024-04-11 11:06:28 +0200

    Add files via upload

---
+++ Kecazaispravit3/Kecazaispravit3/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kecazaispravit3
+{
+
+    class Zrakoplov
+    {
+        private string Naziv;
+        private double SnagaMotora;
+        private int DosegLeta;
+
+
+        public Zrakoplov(string naziv,double motor,int let)
+        {
+            Naziv = naziv;
+            SnagaMotora = motor;
+            DosegLeta = let;
+        }
+
+    }
+    internal class Program
+    {
+        
+
+
+        static void Main(string[] args)
+        {
+            Zrakoplov x360 = new Zrakoplov("Airbus", 2000, 6000);
+            Zrakoplov C162 = new Zrakoplov("Cessna Skycatcher", 75, 870);
+            Zrakoplov PC21 = new Zrakoplov("Pilatus", 1200, 1333);
+        }
+    }
+}
