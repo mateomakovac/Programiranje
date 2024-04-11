98cc53c3a243b5db8a858f12a80382ae1a691c30
mateomakovac <153857861+mateomakovac@users.noreply.github.com>
2024-04-11 10:51:35 +0200

    Add files via upload

---
+++ Kecazaispravit2/Kecazaispravit2/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kecazaispravit2
+{
+      public class Sport
+    {
+        private string Naziv;
+        private bool IgraLiSeLoptom;
+        private byte BrojIgraca;
+        
+    
+        public Sport(string nnaziv)
+    {
+        Naziv = nnaziv;
+    }
+
+    public Sport(string nnaziv,bool igra)
+        {
+            Naziv = nnaziv;
+            IgraLiSeLoptom = igra;
+        }
+
+
+        public Sport(string nnaziv,bool igra,byte igraci)
+        {
+            Naziv = nnaziv;
+            IgraLiSeLoptom = igra;
+            BrojIgraca = igraci;
+        }
+
+
+
+
+}
+
+internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Sport Nogomet = new Sport("Nogomet", true, 20);
+            Sport Vaterpolo = new Sport("Vaterpolo", true, 16);
+            Sport Curling = new Sport("Curling", false, 60);
+        }
+    }
+}
