637aad1d3f35271e4521898237543c3c0f58b264
mateomakovac <153857861+mateomakovac@users.noreply.github.com>
2024-04-21 15:07:05 +0200

    Add files via upload

---
+++ TxtEditor/TxtEditor/Form1.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace TxtEditor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.File = new System.Windows.Forms.ToolStrip();
+            this.boldButton = new System.Windows.Forms.ToolStripButton();
+            this.italicButton = new System.Windows.Forms.ToolStripButton();
+            this.underlineButton = new System.Windows.Forms.ToolStripButton();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.New = new System.Windows.Forms.Button();
+            this.Save = new System.Windows.Forms.Button();
+            this.Open = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.File.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // File
+            // 
+            this.File.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.boldButton,
+            this.italicButton,
+            this.underlineButton});
+            this.File.Location = new System.Drawing.Point(0, 0);
+            this.File.Name = "File";
+            this.File.Size = new System.Drawing.Size(800, 25);
+            this.File.TabIndex = 3;
+            this.File.Text = "toolStrip1";
+            // 
+            // boldButton
+            // 
+            this.boldButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.boldButton.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.boldButton.Name = "boldButton";
+            this.boldButton.Size = new System.Drawing.Size(35, 22);
+            this.boldButton.Text = "Bold";
+            this.boldButton.Click += new System.EventHandler(this.boldButton_Click);
+            // 
+            // italicButton
+            // 
+            this.italicButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.italicButton.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.italicButton.Name = "italicButton";
+            this.italicButton.Size = new System.Drawing.Size(41, 22);
+            this.italicButton.Text = "Italics";
+            this.italicButton.Click += new System.EventHandler(this.italicButton_Click);
+            // 
+            // underlineButton
+            // 
+            this.underlineButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.underlineButton.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.underlineButton.Name = "underlineButton";
+            this.underlineButton.Size = new System.Drawing.Size(62, 22);
+            this.underlineButton.Text = "Underline";
+            this.underlineButton.Click += new System.EventHandler(this.underlineButton_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // New
+            // 
+            this.New.Location = new System.Drawing.Point(13, 359);
+            this.New.Name = "New";
+            this.New.Size = new System.Drawing.Size(75, 23);
+            this.New.TabIndex = 4;
+            this.New.Text = "New";
+            this.New.UseVisualStyleBackColor = true;
+            this.New.Click += new System.EventHandler(this.New_Click);
+            // 
+            // Save
+            // 
+            this.Save.Location = new System.Drawing.Point(115, 359);
+            this.Save.Name = "Save";
+            this.Save.Size = new System.Drawing.Size(75, 23);
+            this.Save.TabIndex = 5;
+            this.Save.Text = "Save";
+            this.Save.UseVisualStyleBackColor = true;
+            this.Save.Click += new System.EventHandler(this.Save_Click);
+            // 
+            // Open
+            // 
+            this.Open.Location = new System.Drawing.Point(217, 359);
+            this.Open.Name = "Open";
+            this.Open.Size = new System.Drawing.Size(75, 23);
+            this.Open.TabIndex = 6;
+            this.Open.Text = "Open\r\n";
+            this.Open.UseVisualStyleBackColor = true;
+            this.Open.Click += new System.EventHandler(this.Open_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(13, 29);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(748, 324);
+            this.richTextBox1.TabIndex = 7;
+            this.richTextBox1.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.Open);
+            this.Controls.Add(this.Save);
+            this.Controls.Add(this.New);
+            this.Controls.Add(this.File);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.File.ResumeLayout(false);
+            this.File.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ToolStrip File;
+        private System.Windows.Forms.ToolStripButton boldButton;
+        private System.Windows.Forms.ToolStripButton italicButton;
+        private System.Windows.Forms.ToolStripButton underlineButton;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.Button New;
+        private System.Windows.Forms.Button Save;
+        private System.Windows.Forms.Button Open;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
+
---
+++ TxtEditor/TxtEditor/Form1.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace TxtEditor
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void boldButton_Click(object sender, EventArgs e)
+        {
+            Font currentFont = richTextBox1.SelectionFont;
+            FontStyle newFontStyle;
+
+            if (currentFont.Bold)
+            {
+                newFontStyle = FontStyle.Regular;
+            }
+            else
+            {
+                newFontStyle = FontStyle.Bold;
+            }
+
+            richTextBox1.SelectionFont = new Font(currentFont.FontFamily, currentFont.Size, newFontStyle);
+        }
+
+        private void italicButton_Click(object sender, EventArgs e)
+        {
+            Font currentFont = richTextBox1.SelectionFont;
+            FontStyle newFontStyle;
+
+            if (currentFont.Italic)
+            {
+                newFontStyle = FontStyle.Regular;
+            }
+            else
+            {
+                newFontStyle = FontStyle.Italic;
+            }
+
+            richTextBox1.SelectionFont = new Font(currentFont.FontFamily, currentFont.Size, newFontStyle);
+        }
+
+        private void underlineButton_Click(object sender, EventArgs e)
+        {
+            Font currentFont = richTextBox1.SelectionFont;
+            FontStyle newFontStyle;
+
+            if (currentFont.Underline)
+            {
+                newFontStyle = FontStyle.Regular;
+            }
+            else
+            {
+                newFontStyle = FontStyle.Underline;
+            }
+
+            richTextBox1.SelectionFont = new Font(currentFont.FontFamily, currentFont.Size, newFontStyle);
+        }
+
+        private void New_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Clear();
+        }
+
+        private void Save_Click(object sender, EventArgs e)
+        {
+            saveFileDialog1.Title = "Save text Files";
+            saveFileDialog1.DefaultExt = "txt";
+            saveFileDialog1.Filter = "Text files (*.txt)|*.txt|All files (*.*)|*.*";
+            saveFileDialog1.FilterIndex = 2;
+            saveFileDialog1.RestoreDirectory = true;
+
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                richTextBox1.SaveFile(saveFileDialog1.FileName);
+            }
+        }
+
+        private void Open_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog
+            {
+                InitialDirectory = @"D:\",
+                Title = "Browse Text Files",
+
+                CheckFileExists = true,
+                CheckPathExists = true,
+
+                DefaultExt = "txt",
+                Filter = "txt files (*.txt)|*.txt",
+                FilterIndex = 2,
+                RestoreDirectory = true,
+
+                ReadOnlyChecked = true,
+                ShowReadOnly = true
+            };
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                richTextBox1.LoadFile(openFileDialog1.FileName);    
+            }
+        }
+    }
+}
