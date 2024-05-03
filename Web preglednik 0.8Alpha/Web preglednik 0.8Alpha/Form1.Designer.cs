dee3ab71309a8a0db6936a1e519c34c19d6236ee
mateomakovac <153857861+mateomakovac@users.noreply.github.com>
2024-05-03 21:31:40 +0200

    Add files via upload

---
+++ Web preglednik 0.8Alpha/Web preglednik 0.8Alpha/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace Web_preglednik_0._8Alpha
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.urlTextBox = new System.Windows.Forms.TextBox();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.backButton = new System.Windows.Forms.Button();
+            this.forwardButton = new System.Windows.Forms.Button();
+            this.homeButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // urlTextBox
+            // 
+            this.urlTextBox.Location = new System.Drawing.Point(13, 13);
+            this.urlTextBox.Name = "urlTextBox";
+            this.urlTextBox.Size = new System.Drawing.Size(618, 20);
+            this.urlTextBox.TabIndex = 0;
+            this.urlTextBox.TextChanged += new System.EventHandler(this.urlTextBox_TextChanged);
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Location = new System.Drawing.Point(13, 39);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(766, 399);
+            this.webBrowser1.TabIndex = 1;
+            // 
+            // backButton
+            // 
+            this.backButton.Location = new System.Drawing.Point(637, 10);
+            this.backButton.Name = "backButton";
+            this.backButton.Size = new System.Drawing.Size(41, 23);
+            this.backButton.TabIndex = 2;
+            this.backButton.Text = "Back";
+            this.backButton.UseVisualStyleBackColor = true;
+            this.backButton.Click += new System.EventHandler(this.backButton_Click);
+            // 
+            // forwardButton
+            // 
+            this.forwardButton.Location = new System.Drawing.Point(684, 10);
+            this.forwardButton.Name = "forwardButton";
+            this.forwardButton.Size = new System.Drawing.Size(52, 23);
+            this.forwardButton.TabIndex = 3;
+            this.forwardButton.Text = "Fowrd";
+            this.forwardButton.UseVisualStyleBackColor = true;
+            this.forwardButton.Click += new System.EventHandler(this.forwardButton_Click);
+            // 
+            // homeButton
+            // 
+            this.homeButton.Location = new System.Drawing.Point(742, 10);
+            this.homeButton.Name = "homeButton";
+            this.homeButton.Size = new System.Drawing.Size(46, 23);
+            this.homeButton.TabIndex = 4;
+            this.homeButton.Text = "Home";
+            this.homeButton.UseVisualStyleBackColor = true;
+            this.homeButton.Click += new System.EventHandler(this.homeButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.homeButton);
+            this.Controls.Add(this.forwardButton);
+            this.Controls.Add(this.backButton);
+            this.Controls.Add(this.webBrowser1);
+            this.Controls.Add(this.urlTextBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox urlTextBox;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.Button backButton;
+        private System.Windows.Forms.Button forwardButton;
+        private System.Windows.Forms.Button homeButton;
+    }
+}
+
---
+++ Web preglednik 0.8Alpha/Web preglednik 0.8Alpha/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace Web_preglednik_0._8Alpha
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void backButton_Click(object sender, EventArgs e)
+        {
+            webBrowser1.GoBack();
+        }
+
+        private void urlTextBox_TextChanged(object sender, EventArgs e)
+        {
+            webBrowser1.Navigate(urlTextBox.Text);
+        }
+
+        private void forwardButton_Click(object sender, EventArgs e)
+        {
+            webBrowser1.GoForward();
+        }
+
+        private void homeButton_Click(object sender, EventArgs e)
+        {
+            webBrowser1.Navigate("http://www.google.com");
+        }
+    }
+}
